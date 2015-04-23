95ce7f58b7d1d39976e2e7bda1e8d1a9e6df7eb3
Nicolas Chaim <nicolas@n1.cl>
2015-04-23 16:21:11 -0500

    Added support for keys on gyro remote.

--- code/GyroInput.cpp
+++ code/GyroInput.cpp
@@ -29,8 +29,7 @@ int GyroInput::connect(void) {
 		for (int j = 0; j < dev->num_classes; j++)
 			if (dev->classes[j]->type == XIValuatorClass) { type = 1; break; }
 		devI[dev->deviceid] = ++devCnt[type];
-		if(type != 1) continue;
-		uint32_t mask = XI_RawMotionMask;
+		uint32_t mask = XI_RawMotionMask  | XI_RawKeyPressMask | XI_RawKeyReleaseMask;
 		XIEventMask evtMask {dev->deviceid, 4, (unsigned char *) &mask};
 		XISelectEvents(dis, win, &evtMask, 1);
 	}
@@ -44,12 +43,16 @@ void GyroInput::capture(void) {
 		XNextEvent(dis, &ev);
 		if (ev.xcookie.type == GenericEvent && ev.xcookie.extension == xi_opcode &&
 				XGetEventData(dis, &ev.xcookie) && listener != nullptr) {
+			XIRawEvent *re =  (XIRawEvent *) ev.xcookie.data;
 			if (ev.xcookie.evtype == XI_RawMotion) {
-				XIRawEvent *re =  (XIRawEvent *) ev.xcookie.data;
 				double dx = 0.0, dy = 0.0, dxr = 0.0, dyr = 0.0;
 				if (re->valuators.mask[0] & 1) { dx = re->valuators.values[0]; dxr = re->raw_values[0]; }
 				if (re->valuators.mask[0] & 2) { dy = re->valuators.values[1]; dyr = re->raw_values[1]; }
 				listener->gyroMoved(devI[re->deviceid], dx, dy, dxr, dyr);
+			} else if (ev.xcookie.evtype == XI_RawKeyPress) {
+				listener->gyroKeyPressed(devI[re->deviceid], re->detail);
+			} else if (ev.xcookie.evtype == XI_RawKeyRelease) {
+				listener->gyroKeyReleased(devI[re->deviceid], re->detail);
 			}
 		}
 		XFreeEventData(dis, &ev.xcookie);
