59bd4ca5d2b6b20a9dcbaf479eabe03c40c9b92c
Nicolas Chaim <nicolas@n1.cl>
2015-05-01 00:46:59 -0500

    Comments on gyro keys

--- code/GyroInput.h
+++ code/GyroInput.h
@@ -5,25 +5,26 @@
 #include <X11/extensions/XInput2.h>
 #include <map>
 
-#define GK_POWER    151
-#define GK_MUTE     121
+#define GK_POWER    151  // *
+#define GK_MUTE     121  // *
 #define GK_UP       111
 #define GK_DOWN     116
 #define GK_LEFT     113
 #define GK_RIGHT    114
 #define GK_ENTER    36
-#define GK_BACK     166
-#define GK_HOME     180
+#define GK_BACK     166  // *
+#define GK_HOME     180  // *
 #define GK_ZOOMOUT  71
 #define GK_ZOOMIN   70
-#define GK_VOLDEC   122
-#define GK_VOLINC   123
+#define GK_VOLDEC   122  // *
+#define GK_VOLINC   123  // *
 #define GK_ESC      9
 #define GK_MENU     135
 #define GK_BACKSP   22
-#define GK_RWD      173
-#define GK_PLAY     172
-#define GK_FWD      171
+#define GK_RWD      173  // *
+#define GK_PLAY     172  // *
+#define GK_FWD      171  // *
+// * Pauses gyro/pointer input while key is pressed
 
 class GyroListener {
 public:
